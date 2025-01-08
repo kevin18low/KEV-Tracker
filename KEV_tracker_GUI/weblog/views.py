c416ff696e8f63a231f34e1e8050785ebe9680c4
Kevin <kevin18low@gmail.com>
2025-01-07 16:59:15 -0800

    Print eventlog on web app

--- KEV_tracker_GUI/weblog/views.py
+++ KEV_tracker_GUI/weblog/views.py
@@ -1,5 +1,33 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import mysql.connector
+
+db = mysql.connector.connect(
+    host="localhost",
+    user="kevin",
+    passwd="klow05_SQL_**",
+    database="CISA-KEV"
+    )   
+print("Connected")
+
+cursor = db.cursor()
+
+cursor.execute("SELECT * FROM Event_log")
+rows = cursor.fetchall()
+
+# Extract column names from cursor.description
+columns = [col[0] for col in cursor.description]
+
+# List to store each log
+logs = []
+
+# Convert each row into a dictionary and append to the list
+for row in rows:
+    row_dict = dict(zip(columns, row))  # Convert row to dictionary
+    logs.append(row_dict)     # Append to the list
 
 def home(request):
-    return HttpResponse('<h1>Event log home page</h1>')
+    context = {
+        'logs': logs
+    }
+    return render(request, 'weblog/home.html', context)
