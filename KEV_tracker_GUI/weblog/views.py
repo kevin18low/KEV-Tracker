e41c58fa210d55f01c1ea7c6ae66a584ff8f4f48
Kevin <kevin18low@gmail.com>
2025-01-08 10:06:14 -0800

    Added live updates and styling to web log

--- KEV_tracker_GUI/weblog/views.py
+++ KEV_tracker_GUI/weblog/views.py
@@ -1,32 +1,30 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 import mysql.connector
 
-db = mysql.connector.connect(
+def home(request):
+    db = mysql.connector.connect(
     host="localhost",
     user="kevin",
     passwd="klow05_SQL_**",
     database="CISA-KEV"
-    )   
-print("Connected")
+)
 
-cursor = db.cursor()
+    cursor = db.cursor()
 
-cursor.execute("SELECT * FROM Event_log")
-rows = cursor.fetchall()
+    cursor.execute("SELECT * FROM Event_log")
+    rows = cursor.fetchall()
 
-# Extract column names from cursor.description
-columns = [col[0] for col in cursor.description]
+    # Extract column names from cursor.description
+    columns = [col[0] for col in cursor.description]
 
-# List to store each log
-logs = []
+    # List to store each log
+    logs = []
 
-# Convert each row into a dictionary and append to the list
-for row in rows:
-    row_dict = dict(zip(columns, row))  # Convert row to dictionary
-    logs.append(row_dict)     # Append to the list
+    # Convert each row into a dictionary and append to the list
+    for row in rows:
+        row_dict = dict(zip(columns, row))  # Convert row to dictionary
+        logs.append(row_dict)     # Append to the list
 
-def home(request):
     context = {
         'logs': logs
     }
